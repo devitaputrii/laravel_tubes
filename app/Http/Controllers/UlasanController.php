63b67ab05295814661f52197679e404983223f88
ramosws03 <ramos.wilfred@gmail.com>
2023-12-31 15:36:06 +0700

    update ulasan ramos

--- app/Http/Controllers/UlasanController.php
+++ app/Http/Controllers/UlasanController.php
@@ -83,7 +83,33 @@ class UlasanController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validate the form data
+        $request->validate([
+            'Rating' => 'required|numeric|min:1|max:5',
+            'JudulUlasan' => 'required|string|max:255',
+            'Ulasan' => 'required|string',
+            // Add other validation rules for additional fields
+        ]);
+
+        // Find the Ulasan model by ID
+        $ulasan = Ulasan::find($id);
+
+        // Check if the Ulasan is found
+        if (!$ulasan) {
+            return redirect()->back()->with('error', 'Ulasan not found');
+        }
+
+        // Update the Ulasan model with the new data
+        $ulasan->rating = $request->input('Rating');
+        $ulasan->judul = $request->input('JudulUlasan');
+        $ulasan->ulasan = $request->input('Ulasan');
+        // Update other fields as needed
+
+        // Save the changes
+        $ulasan->save();
+
+        // Redirect back or to a success page
+        return redirect()->back()->with('success', 'Ulasan updated successfully');
     }
 
     /**
@@ -92,5 +118,13 @@ class UlasanController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function softDelete($id)
+    {
+        $ulasan = Ulasan::findOrFail($id);
+        $ulasan->delete(); // This will set the 'deleted_at' timestamp
+
+        return redirect()->back()->with('success', 'Ulasan soft deleted successfully');
     }
 }
